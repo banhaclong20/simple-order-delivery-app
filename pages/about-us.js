34eea195863fbf605cac12ad1b36a13ce022bdcd
banhaclong20 <banhaclong20@gmail.com>
2019-11-03 20:02:51 -0600

    Updated about us page

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Link from "next/link";
 import ContainerLayout from "../components/_App/ContainerLayout";
-import { Grid, Header, Button, Icon, Image, Segment } from "semantic-ui-react";
+import { Grid, Header, Image, Segment } from "semantic-ui-react";
 import { bannerContent } from "../utils/staticContent";
 
 function AboutUs() {
@@ -23,8 +22,11 @@ function AboutUs() {
                 Our STORY
               </Header>
               <p style={{ fontSize: "1.33em" }}>
-                We'll have your order ready on the Rapid Pick-Up shelf at the
-                time you choose. No lines, no wait.
+                Lusum is a sports brand created with a mission to make a
+                difference in the sports consumables market. Our unique business
+                model is to never pay sponsorships to clubs, leagues and players
+                so that we can offer you the very best balls at much more
+                affordable prices.
               </p>
             </Grid.Column>
             <Grid.Column floated="right" width={6}>
@@ -33,35 +35,26 @@ function AboutUs() {
           </Grid.Row>
           <Grid.Row
             className="grid-container-item"
-            style={{ marginBottom: 30 }}
+            style={{ marginBottom: 30, padding: "10px 0" }}
           >
-            <Grid.Column width={8}>
-              <Header as="h3" style={{ fontSize: "2em" }}>
-                Our FOOD
-              </Header>
-              <p style={{ fontSize: "1.33em" }}>
-                We'll deliver your Vietnamese sandwiches to you at your home or
-                business. Ordering for 10 or more? Click here for more details.
-              </p>
-            </Grid.Column>
-            <Grid.Column floated="right" width={6}>
+            <Grid.Column width={6}>
               <Image
                 rounded
                 size="large"
                 src="../static/assets/our-story.jpg"
               />
             </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column textAlign="center">
-              <Link href="start-order">
-                <Button animated size="huge" inverted color="green">
-                  <Button.Content visible>Start Order</Button.Content>
-                  <Button.Content hidden>
-                    <Icon name="arrow right" />
-                  </Button.Content>
-                </Button>
-              </Link>
+            <Grid.Column width={8} floated="right">
+              <Header as="h3" style={{ fontSize: "2em" }}>
+                Our FOOD
+              </Header>
+              <p style={{ fontSize: "1.33em" }}>
+                Lusum is a sports brand created with a mission to make a
+                difference in the sports consumables market. Our unique business
+                model is to never pay sponsorships to clubs, leagues and players
+                so that we can offer you the very best balls at much more
+                affordable prices.
+              </p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
